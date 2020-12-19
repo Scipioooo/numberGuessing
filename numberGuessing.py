8fbb5ebc84a392d0c88f8b0bc6e4b00fab18ba6e
SullivanLb <sullivan.lb56@gmail.com>
2020-12-19 19:11:20 +0100

    executable added

---
+++ numberGuessing.py
@@ -0,0 +1,20 @@
+import random
+
+print("I have a number between 0 and 100 in mind for you to find it.\r\nPropose a number!")
+numberToGuess = random.randint(0, 100)
+numberTried = -1
+
+while numberToGuess != numberTried:
+    try:
+        numberTried = int(input())
+    except ValueError:
+        print("That's not a number ! Try again.")
+        continue
+    
+    if numberToGuess < numberTried:
+        print("Smaller !")
+    elif numberToGuess > numberTried:
+        print("Greater !")
+
+print("Well done to you you found the number that was",numberToGuess,":)")
+input("Press enter to close program")
